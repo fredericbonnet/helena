e99f15cc24176bafb3b3d6f261a6364b1c985482
Frédéric Bonnet <fredericbonnet@free.fr>
2022-02-22 12:47:44 +0000

    Add word syntax checker

---
+++ src/syntax.spec.ts
@@ -0,0 +1,202 @@
+import { expect } from "chai";
+import { MorphemeType, Script, SyntaxChecker, Word, WordType } from "./syntax";
+import { Parser } from "./parser";
+import { Tokenizer } from "./tokenizer";
+
+const LITERAL = "literal";
+const TUPLE = "(word1 word2)";
+const BLOCK = "{word1 word2}";
+const EXPRESSION = "[word1 word2]";
+const STRING = '"$some [string]"';
+const HERE_STRING = '"""some here "" string"""';
+const TAGGED_STRING = '""TAG\nsome tagged " string\nTAG""';
+const LINE_COMMENT = "# some [{comment";
+const BLOCK_COMMENT = "#{ some [block {comment }#";
+
+const roots = [
+  ["literal", LITERAL],
+  ["tuple", TUPLE],
+  ["block", BLOCK],
+  ["expression", EXPRESSION],
+];
+const qualifiedSources = [
+  ["literal", LITERAL],
+  ["tuple", TUPLE],
+  ["block", BLOCK],
+];
+const monomorphemes = [
+  ["string", STRING],
+  ["here-string", HERE_STRING],
+  ["tagged string", TAGGED_STRING],
+];
+const ignored = [
+  ["line comment", LINE_COMMENT],
+  ["block comment", BLOCK_COMMENT],
+];
+
+describe("SyntaxChecker", () => {
+  let tokenizer: Tokenizer;
+  let parser: Parser;
+  let checker: SyntaxChecker;
+
+  const parse = (script: string) => parser.parse(tokenizer.tokenize(script));
+  const firstWord = (script: Script) => script.sentences[0].words[0];
+
+  beforeEach(() => {
+    tokenizer = new Tokenizer();
+    parser = new Parser();
+    checker = new SyntaxChecker();
+  });
+
+  describe("roots", () => {
+    for (let [type, value] of [...roots, ...monomorphemes]) {
+      specify(type + " root", () => {
+        const script = parse(value);
+        const word = firstWord(script);
+        expect(word.morphemes).to.have.length(1);
+        expect(checker.checkWord(word)).to.eq(WordType.ROOT);
+      });
+    }
+  });
+  describe("compounds", () => {
+    specify("literal prefix", () => {
+      const script = parse(LITERAL + "$" + BLOCK);
+      const word = firstWord(script);
+      expect(word.morphemes).to.have.length(3);
+      expect(checker.checkWord(word)).to.eq(WordType.COMPOUND);
+    });
+    specify("expression prefix", () => {
+      const script = parse(EXPRESSION + LITERAL);
+      const word = firstWord(script);
+      expect(word.morphemes).to.have.length(2);
+      expect(checker.checkWord(word)).to.eq(WordType.COMPOUND);
+    });
+    specify("substitution prefix", () => {
+      const script = parse("$" + BLOCK + TUPLE + LITERAL);
+      const word = firstWord(script);
+      expect(word.morphemes).to.have.length(4);
+      expect(checker.checkWord(word)).to.eq(WordType.COMPOUND);
+    });
+  });
+  describe("substitutions", () => {
+    for (let [type, value] of roots) {
+      describe(type + " source", () => {
+        specify("simple", () => {
+          const script = parse("$" + value);
+          const word = firstWord(script);
+          expect(word.morphemes).to.have.length(2);
+          expect(checker.checkWord(word)).to.eq(WordType.SUBSTITUTION);
+        });
+        specify("indexed selector", () => {
+          const script = parse("$" + value + EXPRESSION);
+          const word = firstWord(script);
+          expect(word.morphemes).to.have.length(3);
+          expect(checker.checkWord(word)).to.eq(WordType.SUBSTITUTION);
+        });
+        specify("keyed selector", () => {
+          const script = parse("$" + value + TUPLE);
+          const word = firstWord(script);
+          expect(word.morphemes).to.have.length(3);
+          expect(checker.checkWord(word)).to.eq(WordType.SUBSTITUTION);
+        });
+        specify("multiple selectors", () => {
+          const script = parse(
+            "$" + value + TUPLE + TUPLE + EXPRESSION + TUPLE + EXPRESSION
+          );
+          const word = firstWord(script);
+          expect(word.morphemes).to.have.length(7);
+          expect(checker.checkWord(word)).to.eq(WordType.SUBSTITUTION);
+        });
+      });
+    }
+  });
+  describe("qualified words", () => {
+    for (let [type, value] of qualifiedSources) {
+      describe(type + " source", () => {
+        specify("indexed selector", () => {
+          const script = parse(value + EXPRESSION);
+          const word = firstWord(script);
+          expect(word.morphemes).to.have.length(2);
+          expect(checker.checkWord(word)).to.eq(WordType.QUALIFIED);
+        });
+        specify("keyed selector", () => {
+          const script = parse(value + TUPLE);
+          const word = firstWord(script);
+          expect(word.morphemes).to.have.length(2);
+          expect(checker.checkWord(word)).to.eq(WordType.QUALIFIED);
+        });
+        specify("multiple selectors", () => {
+          const script = parse(
+            value + TUPLE + TUPLE + EXPRESSION + TUPLE + EXPRESSION
+          );
+          const word = firstWord(script);
+          expect(word.morphemes).to.have.length(6);
+          expect(checker.checkWord(word)).to.eq(WordType.QUALIFIED);
+        });
+      });
+    }
+    describe("exceptions", () => {
+      specify("trailing morphemes", () => {
+        const script = parse(
+          LITERAL + TUPLE + TUPLE + EXPRESSION + TUPLE + EXPRESSION + LITERAL
+        );
+        const word = firstWord(script);
+        expect(word.morphemes).to.have.length(7);
+        expect(() => checker.checkWord(word)).to.throw(
+          "invalid word structure"
+        );
+      });
+    });
+  });
+  describe("ignored words", () => {
+    for (let [type, value] of ignored) {
+      specify(type, () => {
+        const script = parse(value);
+        const word = firstWord(script);
+        expect(word.morphemes).to.have.length(1);
+        expect(checker.checkWord(word)).to.eq(WordType.IGNORED);
+      });
+    }
+  });
+  describe("impossible cases", () => {
+    specify("empty word", () => {
+      const word = new Word();
+      expect(() => checker.checkWord(word)).to.throw("empty word");
+    });
+    specify("empty substitution", () => {
+      const word = new Word();
+      word.morphemes.push({ type: MorphemeType.SUBSTITUTE_NEXT });
+      expect(() => checker.checkWord(word)).to.throw("invalid word structure");
+    });
+    describe("incompatible morphemes", () => {
+      for (let [type1, value1] of [...monomorphemes, ...ignored]) {
+        for (let [type2, value2] of [...roots, ...monomorphemes]) {
+          specify(type1 + "/" + type2, () => {
+            const word = new Word();
+            word.morphemes.push(firstWord(parse(value1)).morphemes[0]);
+            word.morphemes.push(firstWord(parse(value2)).morphemes[0]);
+            expect(() => checker.checkWord(word)).to.throw(
+              "invalid word structure"
+            );
+          });
+          specify(type2 + "/" + type1, () => {
+            const word = new Word();
+            word.morphemes.push(firstWord(parse(value2)).morphemes[0]);
+            word.morphemes.push(firstWord(parse(value1)).morphemes[0]);
+            expect(() => checker.checkWord(word)).to.throw(
+              "invalid word structure"
+            );
+          });
+        }
+      }
+      specify("substitution", () => {
+        const word = new Word();
+        word.morphemes.push({ type: MorphemeType.SUBSTITUTE_NEXT });
+        word.morphemes.push({ type: MorphemeType.BLOCK_COMMENT });
+        expect(() => checker.checkWord(word)).to.throw(
+          "invalid word structure"
+        );
+      });
+    });
+  });
+});
--- src/syntax.ts
+++ src/syntax.ts
@@ -63,3 +63,120 @@ export interface SubstituteNextMorpheme extends Morpheme {
   levels: number;
   value: string;
 }
+
+export enum WordType {
+  ROOT,
+  COMPOUND,
+  SUBSTITUTION,
+  QUALIFIED,
+  IGNORED,
+}
+
+export class SyntaxChecker {
+  checkWord(word: Word): WordType {
+    if (word.morphemes.length == 0) throw new Error("empty word");
+    switch (word.morphemes[0].type) {
+      case MorphemeType.LITERAL:
+      case MorphemeType.EXPRESSION:
+        return this.checkCompoundWord(word);
+      case MorphemeType.TUPLE:
+      case MorphemeType.BLOCK:
+        return this.checkQualifiedWord(word);
+      case MorphemeType.STRING:
+      case MorphemeType.HERE_STRING:
+      case MorphemeType.TAGGED_STRING:
+        return this.checkRootWord(word);
+      case MorphemeType.LINE_COMMENT:
+      case MorphemeType.BLOCK_COMMENT:
+        return this.checkIgnoredWord(word);
+      case MorphemeType.SUBSTITUTE_NEXT:
+        return this.checkSubstitutionWord(word);
+    }
+  }
+
+  private checkRootWord(word: Word): WordType {
+    if (word.morphemes.length != 1) throw new Error("invalid word structure");
+    return WordType.ROOT;
+  }
+
+  private checkCompoundWord(word: Word): WordType {
+    if (word.morphemes.length == 1) return WordType.ROOT;
+    const selectors = this.skipSelectors(word.morphemes, 1);
+    if (selectors == word.morphemes.length) return WordType.QUALIFIED;
+    if (selectors != 1) throw new Error("invalid word structure");
+    this.checkStems(word.morphemes);
+    return WordType.COMPOUND;
+  }
+
+  private checkQualifiedWord(word: Word): WordType {
+    if (word.morphemes.length == 1) return WordType.ROOT;
+    const selectors = this.skipSelectors(word.morphemes, 1);
+    if (selectors == word.morphemes.length) return WordType.QUALIFIED;
+    throw new Error("invalid word structure");
+  }
+
+  private checkSubstitutionWord(word: Word): WordType {
+    if (word.morphemes.length < 2) throw new Error("invalid word structure");
+    let nbStems = this.checkStems(word.morphemes);
+    return nbStems > 1 ? WordType.COMPOUND : WordType.SUBSTITUTION;
+  }
+
+  private checkIgnoredWord(word: Word): WordType {
+    if (word.morphemes.length != 1) throw new Error("invalid word structure");
+    return WordType.IGNORED;
+  }
+
+  private checkStems(morphemes: Morpheme[]): number {
+    let nbStems = 0;
+    let substitute = false;
+    for (let i = 0; i < morphemes.length; i++) {
+      const morpheme = morphemes[i];
+      if (substitute) {
+        switch (morpheme.type) {
+          case MorphemeType.LITERAL:
+          case MorphemeType.TUPLE:
+          case MorphemeType.BLOCK:
+          case MorphemeType.EXPRESSION:
+            i = this.skipSelectors(morphemes, i + 1) - 1;
+            substitute = false;
+            break;
+
+          default:
+            throw new Error("invalid word structure");
+        }
+      } else {
+        switch (morpheme.type) {
+          case MorphemeType.SUBSTITUTE_NEXT:
+            nbStems++;
+            substitute = true;
+            break;
+
+          case MorphemeType.LITERAL:
+          case MorphemeType.EXPRESSION:
+            nbStems++;
+            substitute = false;
+            break;
+
+          default:
+            throw new Error("invalid word structure");
+        }
+      }
+    }
+    return nbStems;
+  }
+
+  private skipSelectors(morphemes: Morpheme[], first: number): number {
+    for (let i = first; i < morphemes.length; i++) {
+      const morpheme = morphemes[i];
+      switch (morpheme.type) {
+        case MorphemeType.TUPLE:
+        case MorphemeType.EXPRESSION:
+          break;
+
+        default:
+          return i;
+      }
+    }
+    return morphemes.length;
+  }
+}
