7f56b8d2a366f97d8767283dd940694ec33ff15e
Frédéric Bonnet <fredericbonnet@free.fr>
2022-11-12 11:04:07 +0000

    Add TAILCALL result code

--- src/core/results.ts
+++ src/core/results.ts
@@ -7,8 +7,9 @@ import { Value, NIL, StringValue } from "./values";
 /** Helena result codes */
 export enum ResultCode {
   OK,
-  YIELD,
   RETURN,
+  TAILCALL,
+  YIELD,
   ERROR,
   BREAK,
   CONTINUE,
@@ -34,14 +35,17 @@ export type Result<T = unknown> = {
 export const OK = <T = unknown>(value: Value, data?: T): Result<T> => {
   return { code: ResultCode.OK, value, data };
 };
+export const RETURN = (value: Value = NIL): Result => {
+  return { code: ResultCode.RETURN, value };
+};
+export const TAILCALL = (value: Value, context?): Result => {
+  return { code: ResultCode.TAILCALL, value, data: context };
+};
 export const YIELD = (value: Value = NIL, state?): Result => {
   return { code: ResultCode.YIELD, value, data: state };
 };
 export const YIELD_BACK = (result: Result, value: Value): Result => {
   return { ...result, value };
-};
-export const RETURN = (value: Value = NIL): Result => {
-  return { code: ResultCode.RETURN, value };
 };
 export const ERROR = (message: string): Result<never> => {
   return { code: ResultCode.ERROR, value: new StringValue(message) };
