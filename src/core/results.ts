b08671e284e522a9547d812c057ea3485767e90d
Frédéric Bonnet <frederic.bonnet@free.fr>
2022-12-28 22:36:51 +0100

    Add custom result codes

--- src/core/results.ts
+++ src/core/results.ts
@@ -4,7 +4,7 @@
 
 import { Value, NIL, StringValue } from "./values";
 
-/** Helena result codes */
+/** Helena standard result codes */
 export enum ResultCode {
   OK,
   RETURN,
@@ -14,10 +14,16 @@ export enum ResultCode {
   CONTINUE,
 }
 
+/** Helena custom result code */
+export interface CustomResultCode {
+  /** Custom code name */
+  name: string;
+}
+
 /** Helena result */
 export type Result<T = unknown> = {
   /** Result code */
-  readonly code: ResultCode;
+  readonly code: ResultCode | CustomResultCode;
 
   /** Result value */
   readonly value: Value;
@@ -31,25 +37,41 @@ export type Result<T = unknown> = {
  */
 
 /* eslint-disable jsdoc/require-jsdoc */
-export const OK = <T = unknown>(value: Value, data?: T): Result<T> => {
-  return { code: ResultCode.OK, value, data };
-};
-export const RETURN = (value: Value = NIL): Result => {
-  return { code: ResultCode.RETURN, value };
-};
-export const YIELD = (value: Value = NIL, state?): Result => {
-  return { code: ResultCode.YIELD, value, data: state };
-};
-export const YIELD_BACK = (result: Result, value: Value): Result => {
-  return { ...result, value };
-};
-export const ERROR = (message: string): Result<never> => {
-  return { code: ResultCode.ERROR, value: new StringValue(message) };
-};
-export const BREAK = (value: Value = NIL): Result => {
-  return { code: ResultCode.BREAK, value };
-};
-export const CONTINUE = (value: Value = NIL): Result => {
-  return { code: ResultCode.CONTINUE, value };
-};
+export const OK = <T = unknown>(value: Value, data?: T): Result<T> => ({
+  code: ResultCode.OK,
+  value,
+  data,
+});
+export const RETURN = (value: Value = NIL): Result => ({
+  code: ResultCode.RETURN,
+  value,
+});
+export const YIELD = (value: Value = NIL, state?): Result => ({
+  code: ResultCode.YIELD,
+  value,
+  data: state,
+});
+export const YIELD_BACK = (result: Result, value: Value): Result => ({
+  ...result,
+  value,
+});
+export const ERROR = (message: string): Result<never> => ({
+  code: ResultCode.ERROR,
+  value: new StringValue(message),
+});
+export const BREAK = (value: Value = NIL): Result => ({
+  code: ResultCode.BREAK,
+  value,
+});
+export const CONTINUE = (value: Value = NIL): Result => ({
+  code: ResultCode.CONTINUE,
+  value,
+});
+export const CUSTOM_RESULT = (
+  code: CustomResultCode,
+  value: Value = NIL
+): Result => ({
+  code,
+  value,
+});
 /* eslint-enable jsdoc/require-jsdoc */
